33581af9390be2ae593738151d9f0ab8041cb889
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-12 15:21:12 +0200

    feat: Add outline for course card

---
+++ src/components/Courses/courses.js
@@ -0,0 +1,16 @@
+import './courses.css';
+
+function Courses() {
+
+  return (
+    <div className='courseCard'>
+        <div className='courseCover'></div>
+        <h3 className='courseTitle'>Course Title</h3>
+        <p className='coursePrice'></p>
+        <div className='courseContent'></div>
+
+    </div>
+  );
+}
+
+export default Courses;
