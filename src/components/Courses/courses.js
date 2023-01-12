9f92ca7f40484d5aee7061892923fbeffa6a6b88
Omar Fahmy <Ofahmy1234@gmail.com>
2023-01-12 15:56:22 +0200

    feat: Add draft of course card component

--- src/components/Courses/courses.js
+++ src/components/Courses/courses.js
@@ -1,13 +1,26 @@
 import './courses.css';
 
 function Courses() {
-
+// get course contents from database
   return (
     <div className='courseCard'>
-        <div className='courseCover'></div>
-        <h3 className='courseTitle'>Course Title</h3>
-        <p className='coursePrice'></p>
-        <div className='courseContent'></div>
+        <div className='courseCover'>
+            <img src="/assets/Carousel/slide2.jpg" alt='course Cover Image'></img>
+        </div>
+        <h3 className='courseTitle text'>Course Title</h3>
+        <p className='coursePrice text'> ????EGP +VAT (22%)</p>
+        <div className='enrollmentStatus '>Not enrolled</div>
+
+        <div className='courseContent text'>
+            <h6>Course Includes</h6>
+            <ul>
+                <li>?? chapter</li>
+                <li>?? videos</li>
+                <li>?? projects</li>
+                <li>Course Certificate</li>
+
+            </ul>
+        </div>
 
     </div>
   );
