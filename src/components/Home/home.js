5f3eecd907bd957fc8c96b7f37dbdc63daf3690c
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-15 04:17:36 +0200

    Feat: Refactor slide show and mini teaser cards to be ready for database entries

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,95 +1,71 @@
 import './home.css';
 import Carousel from 'react-bootstrap/Carousel';
-
+import slideData from './slides';
+import { Fragment, useEffect, useState } from 'react';
+import Footer from '../footer/footer';
+import OutlineCard from '../cards/outlineCard/outlineCard'
+import Grid from '../grid/grid';
 function Home() {
+  const [slides , setSlides] = useState([]);
+  useEffect(()=>{
+    setSlides(slideData)
+  },[])
+  let teaserCardsData = [
+    {
+      title:"Projects",
+      shortDescription:"Look at our selected projects that shows our theme",
+      id:"teaserCards"
+    },
+    {      
+      title:"Services",
+      shortDescription:"Have a look at the collection of services may they be of help to you",
+      id:"teaserCards"
+
+    },
+    {      
+      title:"Courses",
+      shortDescription:"Have a look at the collection of services may they be of help to you",
+      id:"teaserCards"
+
+    },
+    {      
+      title:"Contact",
+      shortDescription:"Have a look at the collection of services may they be of help to you",
+      id:"teaserCards"
+
+    }
+  ]
   return (
+    <div className='contain'>
+
 
     <Carousel className="blackCursor " fade>
-    <Carousel.Item  id="whiteCursor" >
-      <img
-        className="d-block w-100 cut"
-        src="/assets/Carousel/slide1.jpg"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>0 1 &nbsp; i t e r s</h3>
-        <p>An understanding of the iterative process is crucial for successful design.</p>
-      </Carousel.Caption>
-      </Carousel.Item>
+      {slides.map((slide,index) =>(
+      <Carousel.Item key={index}>
+        <img
+          className="d-block w-100 cut"
+          id= {slide.id !== undefined? slide.id : null}
+          src={slide.src}
+          alt={index + "slide"}
+        />
+        <Carousel.Caption>
+          <h3>{slide.quote.title}</h3>
+          <p>{slide.quote.body}</p>
+        </Carousel.Caption>
+        </Carousel.Item>)
+      )}
 
-      <Carousel.Item >
-      <img
-        className="d-block w-100 cut"
-        src="/assets/Carousel/slide4.jpg"
-        alt="second slide"
-      />
-      <Carousel.Caption>
-        <h3>Wood Court</h3>
-        <p>Dedicated spaces for art, furniture, woordwork and sculptures.</p>
-      </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item >
-      <img
-        className="d-block w-100 cut"
-        src="/assets/Carousel/slide2.jpg"
-        alt="third slide"
-      />
-      <Carousel.Caption>
-        <h3>Masa Villa</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item >
-      <img
-        className="d-block w-100 cut"
-        src="/assets/Carousel/slide5.jpg"
-        alt="third slide"
-      />
-      <Carousel.Caption>
-        <h3>Coming soon...</h3>
-        <p></p>
-      </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item >
-      <img
-        className="d-block w-100 cut"
-        src="https://cdn.discordapp.com/attachments/910313432258068522/1054516749149671464/07.png"
-        alt="third slide"
-      />
-      <Carousel.Caption>
-      <h3>Coming soon...</h3>
-        <p></p>
-      </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item >
-      <img
-        className="d-block w-100 cut"
-        src="/assets/Carousel/slide3.jpg"
-        alt="third slide"
-      />
-      <Carousel.Caption>
-        <h3>KOTOB- PROJECT</h3>
-        <p>Product Showcase in different scenarios & color schemes.</p>
-      </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item >
-      <img
-        className="d-block w-100 cut"
-        src="/assets/Carousel/slide6.jpg"
-        alt="third slide"
-      />
-      <Carousel.Caption>
-        <h3>Madrid apartment</h3>
-        <p>Implementing the concept of minimal design.</p>
-      </Carousel.Caption>
-      </Carousel.Item>
 
       </Carousel>
+
+      <Grid 
+      type={OutlineCard}
+      cards={teaserCardsData}
+      gridID="homeGrid"
+      />
+
+      <Footer />
+      </div>
 
   );
 }
