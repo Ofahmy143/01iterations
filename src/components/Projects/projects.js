478bc14f5380c221971d5dcbcbdc1cc7032de0e3
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-15 04:22:26 +0200

    Feat: Create projects page using grid and project card

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,26 +1,75 @@
 
-import { Fragment } from 'react';
+import { Fragment , useState } from 'react';
+import { Link , Route , Routes } from 'react-router-dom';
+import ProjectDetails from './projectDetails/projectDetails';
+import projectData from './projects.json'
+import Grid from '../grid/grid'
+import ProjectCard from '../cards/projectCard/projectCard';
 import './projects.css';
 
 function Project() {
+  const [projects , setProjects] = useState(projectData)
   return (
-    <Fragment>
-    <div className='grid'>
-        <div className='project' >
+    <div className='projects'>
+    <p id="projectPageTitle">Selected Adventures</p>
+    <Grid 
+    type={ProjectCard}
+    cards={projects}
+    gridID={"grid2"}
+    />
+
+
+    </div>
+
+
+  );
+}
+
+export default Project;
+
+
+    /* <div className='grid2'>
+
+      {projects.map((project,index)=>(
+
+    <div className='project' key={index} >
+          <Link to={`/projects/${project.title}`}>
+        <div id="test">
+            <h4> {project.title}</h4>
+            <p>{project.shortDescription}</p>
+          </div>
+          </Link>
+        <img
+        className="d-block w-100 card"
+        src={project.thubmnail}
+        alt={`project ${index}`}
+      />
+        </div>
+
+      ))}
+
+        
+    </div> */
+
+/*
+<div className='project' >
+        <div  id="test">
+            <h4> Lorem Ipsum</h4>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius</p>
+          </div>
         <img
         className="d-block w-100 card"
         src="/assets/Carousel/slide5.jpg"
         alt="First slide"
       />
         </div>
-        <div className='project center' >
-        <img 
-        className="d-block w-100 "
-        src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f743d2133493707.61bf28717243b.jpg"
-        alt="First slide"
-      />
-        </div>
         <div className='project' >
+          <Link to="/projects/project-title">
+        <div  id="test">
+            <h4> Lorem Ipsum</h4>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius</p>
+          </div>
+          </Link>
         <img
         className="d-block w-100 card"
         src="/assets/Carousel/slide4.jpg"
@@ -63,12 +112,4 @@ function Project() {
       />
         </div>
         
-        
-    </div>
-    </Fragment>
-
-
-  );
-}
-
-export default Project;
+        */
