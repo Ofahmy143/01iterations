c10742cab96fab45bf1a248b2b62c70202eb96d8
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-15 04:23:15 +0200

    Fix: faulty links in the react router routes

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,47 +1,41 @@
 import './navBar.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Home from '../Home/home';
+import Contact from '../Contact/contact';
+import { Fragment, useState } from 'react';
 import {
-	BrowserRouter as Router,
+  BrowserRouter as Router,
 	Routes,
 	Route,
 	Link,
 } from 'react-router-dom';
+import Home from '../Home/home';
 import Project from '../Projects/projects';
-import { Fragment } from 'react';
+import Courses from '../Courses/courses';
+import ProjectDetails from '../Projects/projectDetails/projectDetails';
 
 function NavBar() {
+  const [navTitle , setNavTitle] = useState("0 1&nbsp; i t e r s")
   return (
     <Fragment>
 
 
-
     <div className='nav'>
-        <h3>0 1&nbsp; i t e r s</h3>
+        <h3>0 1&nbsp; i t e r s </h3>
         <div className='links'>
             <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/">Courses</Link>
             <Link to="/">About</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Home</Link>
-
-            {/* <a href="/Project">Projects</a>
-            <a href="">Courses</a>
-            <a href="">About</a>
-            <a href="">Careers</a>
-            <a href="">Contact</a> */}
-
-
+            <Link to="/projects">Projects</Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/">Services</Link>
+            <Link to="/contact">Contact</Link>
 
         </div>
     </div>
     <Routes>
         <Route  path ="/" element={<Home/>} />
         <Route  path ="/projects" element={<Project/>} />
-
+        <Route  path ="/courses" element={<Courses/>} />
+        <Route  path ="/contact" element={<Contact />} />
+        <Route  path ="/projects/project-title" element={<ProjectDetails />} />
    </Routes>
 
 </Fragment>
