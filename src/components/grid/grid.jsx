89dcc5585209b25077fc371a07b8468d4ecb43bc
Omar Fahmy <Ofahmy1234@gmail.com>
2023-02-15 04:20:20 +0200

    Feat: Create a grid for all the cards available

---
+++ src/components/grid/grid.jsx
@@ -0,0 +1,25 @@
+import './grid.css';
+import OutlineCard from '../cards/outlineCard/outlineCard';
+import { useState } from 'react';
+function Grid({type , cards , gridID}) {
+  let CardType = type ;
+  return (
+    <div className='grid' id={gridID !== undefined? gridID : null}>
+      {cards.map((card,index) => (
+      <CardType key={index}
+        thumbnail={card.thumbnail}
+        courseTitle={card.title}
+        courseDescription={card.shortDescription}
+        id={card.id}
+        />
+      ))}
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Grid;
